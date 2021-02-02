add1c354ddb32a0ccf7b4303e30b3dc2fe817cb3
Idan Moral <idan22moral@gmail.com>
2021-02-02 22:35:17 +0200

    Implemented the content description + animation

--- src/components/ContentTile/ContentTile.js
+++ src/components/ContentTile/ContentTile.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Card, CardMedia, CardActionArea, CardHeader, IconButton, withStyles } from '@material-ui/core';
+import { Card, Paper, CardMedia, CardActionArea, CardHeader, IconButton, withStyles, Collapse } from '@material-ui/core';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import { normalizeBoolean } from '../../utils/normalize';
 import { FavoriteBorder } from '@material-ui/icons';
+import { positions } from '@material-ui/system';
 
 const styles = (theme) => {
 	const __defines__ = {
@@ -13,11 +14,13 @@ const styles = (theme) => {
 	return {
 		root: {
 			margin: 20,
-			width: __defines__.mediaHeight * (16 / 9)
+			width: __defines__.mediaHeight * (16 / 9),
+			height: __defines__.mediaHeight + __defines__.headerHeight + 32
 		},
 		header: {
 			height: __defines__.headerHeight,
-			backgroundColor: '#7f48ff'
+			backgroundColor: '#7f48ff',
+			direction: 'rtl'
 		},
 		media: {
 			width: __defines__.mediaHeight * (16 / 9),
@@ -30,6 +33,20 @@ const styles = (theme) => {
 		favoriteIcon: {
 			height: __defines__.headerHeight + 12,
 			width: __defines__.headerHeight + 12
+		},
+		collapse: {
+			
+		},
+		descriptionContainer: {
+			width: __defines__.mediaHeight * (16 / 9),
+			height: __defines__.mediaHeight,
+			direction: 'rtl',
+			fontSize: 30,
+			borderRadius: 0,
+			backgroundColor: '#7f48ff'
+		},
+		descriptionText: {
+			margin: 16
 		}
 	};
 };
@@ -39,12 +56,17 @@ class ContentTile extends React.Component {
 		super(props);
 
 		this.state = {
-			isFavorite : normalizeBoolean(this.props.isFavorite)
+			isFavorite	: normalizeBoolean(this.props.isFavorite),
+			hovered		: false
 		};
 	}
 
 	toggleFavorite() {
 		this.setState({isFavorite: !this.state.isFavorite});
+	}
+
+	toggleDescriptionPanel() {
+		this.setState({hovered: !this.state.hovered});
 	}
 
 	render() {
@@ -68,11 +90,18 @@ class ContentTile extends React.Component {
 						</IconButton>
 					}
 					title="דוח סייבר"
-				/>
-				<CardActionArea>
-						<CardMedia className={classes.media}
+					/>
+				<CardActionArea onMouseEnter={()=>this.toggleDescriptionPanel()} onMouseLeave={()=>this.toggleDescriptionPanel()}>
+					<Collapse className={classes.collapse} in={this.state.hovered}>
+						<Card className={classes.descriptionContainer}>
+							<span className={classes.descriptionText}>
+							לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית גולר מונפרר סוברט לורם שבצק יהול.
+							</span>
+						</Card>
+					</Collapse>
+					<CardMedia className={classes.media}
 							image={require(`../../assets/images/${'power-bi-report.png'}`).default}>
-						</CardMedia>
+					</CardMedia>
 				</CardActionArea>
 			</Card>
 		);
