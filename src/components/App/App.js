0660f6550e6a8df5ea7d37c6c2d9dbe79658adfe
Idan Moral <idan22moral@gmail.com>
2021-02-03 21:11:09 +0200

    Demo App.js for this branch (overwrite later)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import ContentTile from '../ContentTile/ContentTile'
+import { SnackbarProvider } from 'notistack'
 
 function App() {
   return (
-    <h1>Hello, World!</h1>
+	<div align="middle">
+		<br></br>
+		<br></br><br></br><br></br><br></br><br></br><br></br>
+	  <SnackbarProvider maxSnack={3} style={{direction:'rtl'}}>
+		<ContentTile />
+	  </SnackbarProvider>
+	</div>
   );
 }
 
