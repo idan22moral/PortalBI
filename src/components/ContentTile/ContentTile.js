7e0192047da85edd111fd8bf816d01e1387ae298
Idan Moral <idan22moral@gmail.com>
2021-02-03 19:26:38 +0200

    Improved layout, style and added chips

--- src/components/ContentTile/ContentTile.js
+++ src/components/ContentTile/ContentTile.js
@@ -1,52 +1,74 @@
 import React from 'react';
-import { Card, Paper, CardMedia, CardActionArea, CardHeader, IconButton, withStyles, Collapse } from '@material-ui/core';
+import { Card, CardMedia, CardActionArea, CardHeader, CardActions, IconButton, withStyles, Collapse, Typography, Chip, Divider } from '@material-ui/core';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import { normalizeBoolean } from '../../utils/normalize';
 import { FavoriteBorder } from '@material-ui/icons';
-import { positions } from '@material-ui/system';
 
 const styles = (theme) => {
 	const __defines__ = {
 		mediaHeight: 200,
-		headerHeight: 20
+		headerTextHeight: 20,
+		grayBackground: '#F3F5F7',
+		headerColor: '#7F48FF',
+		screenRatio : (16 / 9)
 	};
-
+	
+	const componentWidth = __defines__.mediaHeight * __defines__.screenRatio;
+	const headerHeight = __defines__.headerTextHeight + 32;
 	return {
 		root: {
 			margin: 20,
-			width: __defines__.mediaHeight * (16 / 9),
-			height: __defines__.mediaHeight + __defines__.headerHeight + 32
+			width: componentWidth,
+			//height: __defines__.mediaHeight + headerHeight * 2,
+			backgroundColor: __defines__.grayBackground
 		},
 		header: {
-			height: __defines__.headerHeight,
-			backgroundColor: '#7f48ff',
+			height: __defines__.headerTextHeight,
+			backgroundColor: __defines__.headerColor,
 			direction: 'rtl'
 		},
+		headerText: {
+			width: componentWidth - 16 - 40 - 8,
+			'text-overflow': 'ellipsis',
+			overflow: 'hidden',
+			'white-space': 'nowrap'
+		},
 		media: {
-			width: __defines__.mediaHeight * (16 / 9),
+			width: componentWidth,
 			height: __defines__.mediaHeight
 		},
 		favoriteButton: {
-			height: __defines__.headerHeight + 20,
-			width: __defines__.headerHeight + 20
+			height: __defines__.headerTextHeight + 20,
+			width: __defines__.headerTextHeight + 20
 		},
 		favoriteIcon: {
-			height: __defines__.headerHeight + 12,
-			width: __defines__.headerHeight + 12
-		},
-		collapse: {
-			
+			height: __defines__.headerTextHeight + 12,
+			width: __defines__.headerTextHeight + 12
 		},
 		descriptionContainer: {
-			width: __defines__.mediaHeight * (16 / 9),
+			width: componentWidth,
 			height: __defines__.mediaHeight,
-			direction: 'rtl',
 			fontSize: 30,
 			borderRadius: 0,
-			backgroundColor: '#7f48ff'
+			backgroundColor: __defines__.grayBackground
 		},
 		descriptionText: {
-			margin: 16
+			'margin': '8px 16px 16px 16px',
+			height: __defines__.mediaHeight - 32,
+			width: componentWidth - 32,
+			direction:'rtl',
+			overflowWrap: 'anywhere',
+			whiteSpace: 'pre-line',
+			lineHeight: 1.25,
+			fontSize: 24,
+		},
+		footer: {
+			direction: 'rtl',
+			backgroundColor: __defines__.grayBackground,
+			padding: '12px 16px 12px 16px'
+		},
+		chip: {
+			marginLeft: '4px'
 		}
 	};
 };
@@ -75,6 +97,7 @@ class ContentTile extends React.Component {
 		return (
 			<Card className={classes.root}>
 				<CardHeader
+					titleTypographyProps={{className:classes.headerText}}
 					className={classes.header}
 					action={
 						<IconButton aria-label="settings" className={classes.favoriteButton}
@@ -89,20 +112,29 @@ class ContentTile extends React.Component {
 							}
 						</IconButton>
 					}
-					title="דוח סייבר"
+					title="ליגת מיטב - סיכום מדדים"
 					/>
 				<CardActionArea onMouseEnter={()=>this.toggleDescriptionPanel()} onMouseLeave={()=>this.toggleDescriptionPanel()}>
-					<Collapse className={classes.collapse} in={this.state.hovered}>
-						<Card className={classes.descriptionContainer}>
-							<span className={classes.descriptionText}>
-							לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית גולר מונפרר סוברט לורם שבצק יהול.
-							</span>
-						</Card>
-					</Collapse>
-					<CardMedia className={classes.media}
-							image={require(`../../assets/images/${'power-bi-report.png'}`).default}>
-					</CardMedia>
+					<Card align="right" elevation={0} className={classes.descriptionContainer}>
+						<Collapse in={this.state.hovered}>
+							<CardMedia className={classes.media}
+								image={require(`../../assets/images/${'power-bi-report.png'}`).default}>
+							</CardMedia>
+						</Collapse>
+						<Typography component="pre" className={classes.descriptionText}>
+							{`סיכום מדדי ליגת מיטב
+							ציון כולל המורכב מאחוז מסיימי הליכים בהתייצבות ראשונה,
+							סה"כ זמני שהייה וציון מדדי איכות`}
+						</Typography>
+					</Card>
 				</CardActionArea>
+				<Divider/>
+				<CardActions disableSpacing="true" elevation={0} className={classes.footer}>
+				<Chip className={classes.chip} variant="outlined" size="small" label="#ליגת מיטב" color="primary"/>
+				<Chip className={classes.chip} variant="outlined" size="small" label="#ליגת מיטב" color="primary"/>
+				<Chip className={classes.chip} variant="outlined" size="small" label="#ליגת מיטב" color="primary"/>
+				<Chip className={classes.chip} variant="outlined" size="small" label="#ליגת מיטב" color="primary"/>
+				</CardActions>
 			</Card>
 		);
 	}
